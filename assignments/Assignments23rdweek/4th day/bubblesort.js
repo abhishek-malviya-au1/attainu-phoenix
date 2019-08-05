82519c23d0ab933cfffee74da06bc171a0bfbb9a
abhishek-malviya-au1 <abhishekmalviya17@gmail.com>
2019-08-05 12:28:03 +0530

    bubble sort

--- assignments/Assignments23rdweek/4th day/bubblesort.js
+++ assignments/Assignments23rdweek/4th day/bubblesort.js
@@ -1,13 +1,20 @@
 function randomArray(max){
     let arr=[];
+    let newarr=[]
     for(let i=1 ; i<=max ; i++){
         arr.push(Math.floor(Math.random()*10));
     }
-    return arr;
+    for(let j=0 ; j<arr.length ; j++){
+        if(arr[j]%2!==0){
+            newarr.push(arr[j]);
+        }
+    }
+    return newarr
 }
 
 function bubbleSort(arr){
     let steps=0;
+    let swappingHappened=false;
     for(let i=0 ; i < arr.length ; i++){
         for(let j=0 ; j < arr.length-1; j++ ){
             steps+=1;
@@ -15,14 +22,18 @@ function bubbleSort(arr){
                 let tmp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=tmp;
+                swappingHappened=true;
             }
+        }
+        if(!swappingHappened){
+            break;
         }
     }
     console.log('Steps: ',steps);
     return arr;
 }
 
-let myArray=randomArray(12);
+let myArray=randomArray(15);
 
 console.log(myArray);
 console.log('Sorting..');
